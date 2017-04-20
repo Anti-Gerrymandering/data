debb772cb74c3ad591de31fbd593ebf408d108f6
Jonathan Mast <jon@jonmast.com>
2017-04-20 17:34:27 -0400

    Add compactness calculation script
    This script is copied from
    https://github.com/tcfuji/anti-gerry-compactness, thanks to @tcfuji for
    this.

---
+++ compactness/compactness.py
@@ -0,0 +1,33 @@
+"""
+run `python compactness.py path/to/geojson_files/*.geojson`
+"""
+
+import numpy as np
+import geopandas as gp
+import json
+
+from sys import argv
+from glob import glob
+
+"""
+Parameters
+---------
+geo_df : Geopandas dataframe
+
+Returns
+-------
+compactness : Geopandas dataframe
+"""
+def compactness(geo_df):
+    return (4 * np.pi * geo_df.area) / np.power(geo_df.length, 2)
+
+if __name__ == "__main__":
+    list_of_geojson_files = glob(argv[1])
+    for f in list_of_geojson_files:
+        set_of_geos = gp.read_file(f)
+        compactness_series = compactness(set_of_geos)
+        # print(compactness_df)
+        set_of_geos.loc[:, 'Compactness'] = compactness_series
+        gjson = set_of_geos.to_json()
+        with open(f, 'w') as gjson_file:
+            json.dump(gjson, gjson_file)
